b5aaf7c0a8510ec29beb9d675c7b839cf3c80b15
dsgalabe <davide.sgalaberna@cern.ch>
2016-04-05 19:29:52 +0200

    Remove old directory structure

--- SourceGEANT4.sh
+++
@@ -1,6 +0,0 @@
-
-
-#source /afs/cern.ch/user/d/dsgalabe/GEANT4/v10r1p03_dataON/geant4.10.01.p03-build/geant4make.sh # env var of G4 (e.g. data variables...)
-source /afs/cern.ch/user/d/dsgalabe/GEANT4/v10r1p03_dataON/geant4.10.01.p03-install/bin/geant4.sh
-
-source ~/T2K/work/v11r31/ROOT/v5r34p09n04/amd64_linux26/bin/thisroot.sh # root
