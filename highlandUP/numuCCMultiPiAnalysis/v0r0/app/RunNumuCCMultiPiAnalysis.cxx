c438938c4b48cff768a9a93136a45f8dd6268aeb
tovsyannikova <ovsyanik@inr.ru>
2016-12-20 16:50:18 +0400

    remove numuCCMultiPiAnalysis

--- highlandUP/numuCCMultiPiAnalysis/v0r0/app/RunNumuCCMultiPiAnalysis.cxx
+++
@@ -1,8 +0,0 @@
-#include "numuCCMultiPiAnalysis.hxx"
-#include "AnalysisLoop.hxx"
-
-int main(int argc, char *argv[]){
-  numuCCMultiPiAnalysis* ana = new numuCCMultiPiAnalysis();
-  AnalysisLoop loop(ana, argc, argv); 
-  loop.Execute();
-}
