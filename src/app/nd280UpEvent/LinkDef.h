c7f5456fc89bae2bda4476425eb67aeef58ce66a
dsgalabe <davide.sgalaberna@cern.ch>
2017-04-28 11:31:01 +0200

    LinkDef file for new TND280UpHit class

--- src/app/nd280UpEvent/LinkDef.h
+++ src/app/nd280UpEvent/LinkDef.h
@@ -16,4 +16,7 @@
 #pragma link C++ class TND280UpTrackPoint+;
 #pragma link C++ class std::vector<TND280UpTrackPoint>+;
 
+#pragma link C++ class TND280UpHit+;
+#pragma link C++ class std::vector<TND280UpHit>+;
+
 #endif
