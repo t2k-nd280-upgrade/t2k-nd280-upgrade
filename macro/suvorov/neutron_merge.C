c17640b265142569a538314d1e666d08c39c8e55
suvorov21 <suvorov@inr.ru>
2018-09-18 15:58:42 +0200

    update neutron study

--- macro/suvorov/neutron_merge.C
+++ macro/suvorov/neutron_merge.C
@@ -1,15 +1,39 @@
 #include "TFile.h"
+#include "TTree.h"
 #include "TH2F.h"
 #include "TF1.h"
 #include "TStyle.h"
 #include "TPad.h"
 #include "TGraphAsymmErrors.h"
 #include "TCanvas.h"
+#include "TRandom3.h"
 
 #include <iostream>
 
 void neutron_merge() {
-	TFile* file = new TFile("/t2k/users/suvorov/AnalysisResults/ndUP/SuperFGD/SuperFGD-neutron_v3-UseXY-UseXZ-UseYZ-Separate10_na_200000.root", "READ");
+	TFile* file = new TFile("/t2k/users/suvorov/AnalysisResults/ndUP/SuperFGD/SuperFGD-neutron_v7-UseXY-UseXZ-UseYZ-Separate10_na_1000000.root", "READ");
+
+  const Int_t Ndist = 4;
+  float distance_cut[Ndist];
+  distance_cut[0] = 40.;
+  distance_cut[1] = 50.;
+  distance_cut[2] = 60.;
+  distance_cut[3] = 70.;
+
+  TTree* tree       = (TTree*)file->Get("neutron");
+  Double_t ekin, costheta, light, dist_cubes, dist, first_hit_time, neutron_time, neutron_dist_true;
+  tree->SetBranchAddress("KinEnergy_true",     &ekin);
+  tree->SetBranchAddress("CosTheta_true",      &costheta);
+  tree->SetBranchAddress("Light",              &light);
+  // reco info 
+  tree->SetBranchAddress("Distance_cubes",     &dist_cubes);
+  tree->SetBranchAddress("First_hit_time",     &first_hit_time);
+  // true distance (no smearing with cube size)
+  tree->SetBranchAddress("Distance_hit_true",  &dist);
+  
+  // true neutron points
+  tree->SetBranchAddress("Neutron_time",       &neutron_time);
+  tree->SetBranchAddress("Neutron_dist",       &neutron_dist_true);
 
 	TH2F* init_e_cos  = (TH2F*)file->Get("ini_ET");
 	TH2F* eff_e_cos   = (TH2F*)file->Get("eff_ET");
@@ -25,110 +49,168 @@ void neutron_merge() {
 
   TH2F* pe_e        = (TH2F*)file->Get("pe_E");
 
+  // do rebinning
   Int_t rebin_Y = 5;
-
-  TH2F* energy_resol[6];
-  TH2F* energy_resol_rebin[6];
-
-  TGraphAsymmErrors* graph[6];
-
-  const Int_t Nbins = 28;
-  Double_t bins[Nbins] = {0., 5., 10., 15., 20., 30., 40., 50., 60., 70., 80., 90., 100., 110., 120., 130., 140., 150., 170., 190., 210., 230., 250., 300., 350., 400., 450., 500.};
-
-  TString histo_name;
-  for (Int_t i = 1; i <= 6; ++i) {
-    histo_name = Form("energy_resol%i", i);
-    energy_resol[i-1] = (TH2F*)file->Get(histo_name);
-    //energy_resol[i-1]->RebinY(2);
-    //energy_resol[i-1]->RebinX(2);
-    energy_resol_rebin[i-1] = new TH2F(Form("energy_resol_rebin%i", i), "", Nbins-1, bins, energy_resol[i-1]->GetYaxis()->GetNbins(), 0., 500.);
-    for (Int_t xbin = 0; xbin <= energy_resol[i-1]->GetXaxis()->GetNbins(); ++xbin) {
-      for (Int_t ybin = 0; ybin <= energy_resol[i-1]->GetYaxis()->GetNbins(); ++ybin) {
-        Double_t x_center = energy_resol[i-1]->GetXaxis()->GetBinCenter(xbin);
-        Double_t y_center = energy_resol[i-1]->GetYaxis()->GetBinCenter(ybin);
-        energy_resol_rebin[i-1]->Fill(x_center, y_center, energy_resol[i-1]->GetBinContent(xbin, ybin));
-      }
-    }
-  }
+  init_e_cos->RebinY(rebin_Y);
+  pe_e_cos->RebinY(rebin_Y);
+  eff_e_cos->RebinY(rebin_Y);
+  mom_forward->Rebin(rebin_Y);
+  mom_norm->Rebin(rebin_Y);
 
   TFile* file_out = new TFile("/t2k/users/suvorov/AnalysisResults/ndUP/SuperFGD/neutron/plot3_gamma_VA3.root", "RECREATE");
   file_out->cd();
 
-  TF1* f1 = new TF1("f1", "( (x < [1] ) ? [0] * TMath::Gaus(x, [1], [2]) : [0] * TMath::Gaus(x, [1], [3]))", 0, 500);
-  f1->SetParName(0, "Const");
-  f1->SetParName(1, "Mean");
-  f1->SetParName(2, "Left sigma");
-  f1->SetParName(3, "Right sigma");
-  /*
+  // read tree several times in order to check
+  for (Int_t distID = 0; distID < Ndist; ++distID) {
 
-  for (Int_t resID = 1; resID <= 6; ++resID ) {
-    TH1D* fitted_histo[Nbins];
-    graph[resID-1] = new TGraphAsymmErrors(); 
-    gStyle->SetOptFit(1);
-    for (Int_t i = 0; i < Nbins -1; ++i) {
-      fitted_histo[i] = energy_resol_rebin[resID-1]-> ProjectionY(Form("projections%i_%i", resID, i), i+1, i+1);
-      if (i > 8)
-        fitted_histo[i]->Rebin(2);
-      if (i > 18 && i != 27)
-        fitted_histo[i]->Rebin(2);
+    TH2F* energy_resol[7];
+    TH2F* energy_resol_rebin[7];
   
-      f1->SetParameters(200, 70, 10, 80);
-      fitted_histo[i]->Fit("f1");
-      fitted_histo[i]->Draw();
-      gPad->Update();
-      if (resID == 1)
-        fitted_histo[i]->Write();
+    const Int_t Nbins = 28;
+    Double_t bins[Nbins] = {0., 5., 10., 15., 20., 30., 40., 50., 60., 70., 80., 90., 100., 110., 120., 130., 140., 150., 170.  , 190., 210., 230., 250., 300., 350., 400., 450., 500.};
   
-      TF1 *fit = fitted_histo[i]->GetFunction("f1");
+    for (Int_t i = 0; i < 7; ++i) {
+      energy_resol[i] = new TH2F(Form("resol_mac_%i", i), "energy resolution", 250, 0., 500, 300, 0., 600);
+      energy_resol_rebin[i] = new TH2F(Form("energy_resol_rebin_%i", i), "", Nbins-1, bins, 300, 0., 600.);
+    }
   
-      if (!fit || i == 0) continue;
+    eff_e_cos->Reset();
   
-      Float_t mean      = fit->GetParameter(1);
-      Float_t sigmaL    = fit->GetParameter(2);
-      Float_t sigmaR    = fit->GetParameter(3);
-
-      if (sigmaL < 0)
-        sigmaL = 0;
-      if (sigmaR > 500)
-        sigmaR = 0;
-
-      if (mean < 0)
+    // loop over tree to fill the resolution histoes
+    float beta, time, ekin2;
+    TRandom3* gen = new TRandom3();
+    Int_t NEntries = tree->GetEntries();
+    for (Int_t entryID = 0; entryID < NEntries; ++entryID) {
+      tree->GetEntry(entryID);
+  
+      // selection on distance (cm)
+      if (dist_cubes < distance_cut[distID])
         continue;
   
-      Double_t x_center = energy_resol_rebin[resID-1]->GetXaxis()->GetBinCenter(i);
+      eff_e_cos->Fill(costheta, ekin);
   
-      graph[resID-1]->SetPoint(graph[resID-1]->GetN(), x_center, mean);
-      graph[resID-1]->SetPointError(graph[resID-1]->GetN()-1, 0., 0., sigmaL, sigmaR);
+      //beta = neutron_dist_true / (neutron_time * 30.);
+      beta = dist_cubes / (first_hit_time * 30.);
+      ekin2 = sqrt(939.565379*939.565379 / (1 - beta*beta)) - 939.565379;
+      energy_resol[0]->Fill(ekin, ekin2);
+      energy_resol_rebin[0]->Fill(ekin, ekin2);
+  
+      time = gen->Gaus(first_hit_time, 0.9);
+      beta = dist_cubes / (time * 30.);
+      ekin2 = sqrt(939.565379*939.565379 / (1 - beta*beta)) - 939.565379;
+      energy_resol[1]->Fill(ekin, ekin2);
+      energy_resol_rebin[1]->Fill(ekin, ekin2);
+  
+      time = gen->Gaus(first_hit_time, 0.9/sqrt(2));
+      beta = dist_cubes / (time * 30.);
+      ekin2 = sqrt(939.565379*939.565379 / (1 - beta*beta)) - 939.565379;
+      energy_resol[2]->Fill(ekin, ekin2);
+      energy_resol_rebin[2]->Fill(ekin, ekin2);
+  
+      time = gen->Gaus(first_hit_time, 0.9/sqrt(3));
+      beta = dist_cubes / (time * 30.);
+      ekin2 = sqrt(939.565379*939.565379 / (1 - beta*beta)) - 939.565379;
+      energy_resol[3]->Fill(ekin, ekin2);
+      energy_resol_rebin[3]->Fill(ekin, ekin2);
+  
+      time = gen->Gaus(first_hit_time, 0.6);
+      beta = dist_cubes / (time * 30.);
+      ekin2 = sqrt(939.565379*939.565379 / (1 - beta*beta)) - 939.565379;
+      energy_resol[4]->Fill(ekin, ekin2);
+      energy_resol_rebin[4]->Fill(ekin, ekin2);
+  
+      time = gen->Gaus(first_hit_time, 0.6/sqrt(2));
+      beta = dist_cubes / (time * 30.);
+      ekin2 = sqrt(939.565379*939.565379 / (1 - beta*beta)) - 939.565379;
+      energy_resol[5]->Fill(ekin, ekin2);
+      energy_resol_rebin[5]->Fill(ekin, ekin2);
+  
+      time = gen->Gaus(first_hit_time, 0.6/sqrt(3));
+      beta = dist_cubes / (time * 30.);
+      ekin2 = sqrt(939.565379*939.565379 / (1 - beta*beta)) - 939.565379;
+      energy_resol[6]->Fill(ekin, ekin2);
+      energy_resol_rebin[6]->Fill(ekin, ekin2);
     }
   
-    graph[resID-1]->SetMaximum(500.);
-    graph[resID-1]->SetMinimum(0.);
-    graph[resID-1]->Draw("ap");
-    gPad->Update();
-    graph[resID-1]->Write(Form("graph%i", resID));
-  }
-  */
+    TGraphAsymmErrors* graph[6];
+  
+    TF1* f1 = new TF1("f1", "( (x < [1] ) ? [0] * TMath::Gaus(x, [1], [2]) : [0] * TMath::Gaus(x, [1], [3]))", 0, 700);
+    f1->SetParName(0, "Const");
+    f1->SetParName(1, "Mean");
+    f1->SetParName(2, "Left sigma");
+    f1->SetParName(3, "Right sigma");
+    
+    TDirectory* dir = file_out->mkdir(Form("CutValue_%f", distance_cut[distID]));
+    dir->cd();
+    
+    // loop over projections to fit
+    for (Int_t resID = 0; resID < 7; ++resID ) {
+      TH1D* fitted_histo[Nbins+5];
+      graph[resID] = new TGraphAsymmErrors(); 
+      gStyle->SetOptFit(1);
+      for (Int_t i = 2; i <= Nbins; ++i) {
+        fitted_histo[i] = energy_resol_rebin[resID]-> ProjectionY(Form("projections_%i_%i", resID, i), i, i);
+        if (i > 8 && i < 19)
+          fitted_histo[i]->Rebin(2);
+        if (i > 18)
+          fitted_histo[i]->Rebin(5);
+  
+        float pre_mean = energy_resol_rebin[resID]->GetXaxis()->GetBinCenter(i);
+    
+        f1->SetParameters(100, pre_mean, pre_mean*0.1, pre_mean*0.1);
+        fitted_histo[i]->Fit("f1", "Q");
+        fitted_histo[i]->Draw();
+        gPad->Update();
+        /*if (resID == 2)
+          fitted_histo[i]->Write();*/
+    
+        TF1 *fit = fitted_histo[i]->GetFunction("f1");
+    
+        if (!fit) continue;
+    
+        Float_t mean      = fit->GetParameter(1);
+        Float_t sigmaL    = fit->GetParameter(2);
+        Float_t sigmaR    = fit->GetParameter(3);
+  
+        if (sigmaL < 0)
+          sigmaL = 0;
+        if (sigmaR > 500)
+          sigmaR = 0;
+  
+        if (mean < 0)
+          continue;
+    
+        Double_t x_center = energy_resol_rebin[resID]->GetXaxis()->GetBinCenter(i);
+    
+        graph[resID]->SetPoint(graph[resID]->GetN(), x_center, mean);
+        graph[resID]->SetPointError(graph[resID]->GetN()-1, 0., 0., sigmaL, sigmaR);
+        std::cout << graph[resID]->GetN() << "\t" << x_center << "\t" << mean << "\t" << sigmaL << "\t" << sigmaR << "\t" <<  sigmaR / mean *100. << std::endl;
+      }
+    
+      graph[resID]->SetMaximum(600.);
+      graph[resID]->SetMinimum(0.);
+      graph[resID]->Draw("ap");
+      gPad->Update();
+      graph[resID]->Write(Form("graph%i", resID));
+    } // loop over projections to fit
+    // draw resolution and efficiency for each distance
+    TH2F* eff_e_cos_2 = (TH2F*)eff_e_cos->Clone("eff_ecos2");
 
-	// do rebinning
-	
-	init_e_cos->RebinY(rebin_Y);
-	pe_e_cos->RebinY(rebin_Y);
-	eff_e_cos->RebinY(rebin_Y);
-  mom_forward->Rebin(rebin_Y);
-  mom_norm->Rebin(rebin_Y);
-
-	TH2F* eff_e_cos_2 = (TH2F*)eff_e_cos->Clone("eff_ecos2");
-
-	std::cout << "cos theta bins \t" << init_e_cos->GetXaxis()->GetNbins() << std::endl;
-	std::cout << "energy bins \t" << init_e_cos->GetYaxis()->GetNbins() << std::endl;
-
-	for (Int_t i = 1; i <= init_e_cos->GetXaxis()->GetNbins(); ++i) {
-    for (Int_t j = 1; j <= init_e_cos->GetYaxis()->GetNbins(); ++j) {
-      Float_t pre = eff_e_cos->GetBinContent(i, j);
-      Float_t scale = init_e_cos->GetBinContent(i, j);
-      eff_e_cos_2->SetBinContent(i, j, pre / scale);
+    for (Int_t i = 1; i <= init_e_cos->GetXaxis()->GetNbins(); ++i) {
+      for (Int_t j = 1; j <= init_e_cos->GetYaxis()->GetNbins(); ++j) {
+        Float_t pre = eff_e_cos->GetBinContent(i, j);
+        Float_t scale = init_e_cos->GetBinContent(i, j);
+        eff_e_cos_2->SetBinContent(i, j, pre / scale);
+      }
     }
-  }
+    eff_e_cos_2->Write();
+
+    for (Int_t i = 0; i < 7; ++i) {
+      energy_resol[i]->Write();
+      energy_resol_rebin[i]->Write();
+    }
+
+  } // loop over distance cuts
 
   for (Int_t i = 1; i <= pe_e_cos->GetXaxis()->GetNbins(); ++i) {
     for (Int_t j = 1; j <= pe_e_cos->GetYaxis()->GetNbins(); ++j) {
@@ -137,26 +219,26 @@ void neutron_merge() {
       pe_e_cos->SetBinContent(i, j, pre / scale);
     }
   }
-
+  file_out->cd();
   // energy distance study
   // TH2F* e_dist      = new TH2F("e_dist", "Energy vs distance", 300, 0., 1500, 250, 0., 500.);
   TH2F* e_d_c = (TH2F*)e_dist->Clone("e_d_c");
-  TH1D* dist = e_d_c->ProjectionX("dist", 13, -1);
-  dist->Rebin(5);
-  dist->SetTitle("First hit distance. Ekin > 26 MeV");
-  dist->Write();
-  TH1D* dist_eff = (TH1D*)dist->Clone("Energy distance");
+  TH1D* dist_proj = e_d_c->ProjectionX("dist", 13, -1);
+  dist_proj->Rebin(5);
+  dist_proj->SetTitle("First hit distance. Ekin > 26 MeV");
+  dist_proj->Write();
+  TH1D* dist_eff = (TH1D*)dist_proj->Clone("Energy distance");
   dist_eff->SetTitle("Efficiency vs distance cut");
-  for (Int_t i = 1; i <= dist->GetNbinsX(); ++i) {
-    dist_eff->SetBinContent(i, dist->Integral(i, -1));
+  for (Int_t i = 1; i <= dist_proj->GetNbinsX(); ++i) {
+    dist_eff->SetBinContent(i, dist_proj->Integral(i, -1));
   }
-  dist_eff->Scale(1./dist->Integral());
+  dist_eff->Scale(1./dist_proj->Integral());
   dist_eff->Write();
 
   mom_forward->Divide(mom_norm);
 
-  TF1* beta = new TF1("beta", "sqrt(x*x + 2*x*939)/(x+939)", 0., 500.);
-  (void)beta;
+  TF1* beta_f = new TF1("beta", "sqrt(x*x + 2*x*939)/(x+939)", 0., 500.);
+  (void)beta_f;
   TCanvas* c1 = new TCanvas("canva","",50,50,1000,800);
   TF1* resol_100_05 = new TF1("resol_100_09", "1/x * 939* TMath::Power((1-beta(x)*beta(x)), -1.5) * TMath::Power(beta(x), 3)/100*0.5*30", 0., 500.);
   resol_100_05->Draw();
@@ -178,7 +260,6 @@ void neutron_merge() {
   resol_50_03->Draw("same");
   c1->Write();
 
-  //TF1* resol_20_09 = new TF1("resol_20_09", "1/x * 939* TMath::Power((1-beta(x)*beta(x)), -1.5) * TMath::Power(beta(x), 3)/20*0.9*30", 0., 500.);
   TF1* resol_20_07 = new TF1("resol_20_07", "1/x * 939* TMath::Power((1-beta(x)*beta(x)), -1.5) * TMath::Power(beta(x), 3)/20*0.7*30", 0., 500.);
   resol_20_07->SetLineColor(kGreen);
   TF1* resol_20_05 = new TF1("resol_20_05", "1/x * 939* TMath::Power((1-beta(x)*beta(x)), -1.5) * TMath::Power(beta(x), 3)/20*0.5*30", 0., 500.);
@@ -193,7 +274,6 @@ void neutron_merge() {
 
   init_e_cos->Write();
   eff_e_cos->Write();
-  eff_e_cos_2->Write();
   pe_e_cos->Write();
   pe_e->Write();
   hits_energy->Write();
@@ -206,11 +286,6 @@ void neutron_merge() {
   gPad->Modified();
   gPad->Update();
   mom_forward->Write();
-
-  for (Int_t i = 1; i <= 6; ++i) {
-    energy_resol[i-1]->Write();
-    energy_resol_rebin[i-1]->Write();
-  }
 
   file_out->Close();
   file->Close();
