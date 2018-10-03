a6c128ac79eed7a14c814b5b33203ca0aeff2e64
suvorov21 <suvorov@inr.ru>
2018-10-03 16:06:14 +0200

    Massive cleanup. Remove all root and wrl files. Remove autogenerated cmakefiles. Add both to .gitignore.

--- T2KND280Up_CLHEP.sh
+++ T2KND280Up_CLHEP.sh
@@ -1,45 +1,46 @@
+#!bin/sh
 
+# Specify all the environment variables here!
+export GEANT4_DIR=/Users/suvorov/SOFT/CERN/GEANT4/geant4.10.01.p03-install/
+export CLHEP_DIR=/Users/suvorov/SOFT/CERN/CLHEP/2.4.0.4-install/
+export CMT_DIR=/Users/suvorov/SOFT/OTHER/CMT/
+export ROOT_DIR=/Users/suvorov/SOFT/CERN/ROOT/root-6.12.06-build/
 
 # setup GEANT4
-#source /atlas/users/dsgalabe/GEANT4/geant4.10.01.p03-build/geant4make.sh # env var of G4 (e.g. data variables...)
-source /atlas/users/dsgalabe/GEANT4/geant4.10.01.p03-install/bin/geant4.sh
-
-export GEANT4_INCLUDE=/atlas/users/dsgalabe/GEANT4/geant4.10.01.p03-install/include/Geant4/
-export GEANT4_INSTALL_BIN=/atlas/users/dsgalabe/GEANT4/geant4.10.01.p03-install/bin/
-export GEANT4_LIBRARY=/atlas/users/dsgalabe/GEANT4/geant4.10.01.p03-install/lib64
+source $GEANT4_DIR/bin/geant4.sh
+export GEANT4_INCLUDE=$GEANT4_DIR/include/Geant4/
+export GEANT4_INSTALL_BIN=$GEANT4_DIR/bin/
+export GEANT4_LIBRARY=$GEANT4_DIR/lib/
+export GEANT4_LIBRARY_DIR=$GEANT4_DIR/lib/
 
 # setup ROOT
-source /atlas/users/dsgalabe/ROOT/root/bin/thisroot.sh
+source $ROOT_DIR/bin/thisroot.sh
 
- # setup CMT
-source /atlas/users/dsgalabe/CMT/setup.sh
+# setup CMT
+source $CMT_DIR/setup.sh
 export CMTPATH=$1
-
-# # setup highlandUp
-#source /atlas/users/dsgalabe/t2k-nd280-upgrade/highlandUP/ndupAnalysis/v0r0/cmt/setup.sh
-#source /atlas/users/dsgalabe/t2k-nd280-upgrade/highlandUP/highlandTools/v0r0/cmt/setup.sh 
-
-# setup gcc-xml
-#export PATH=/afs/cern.ch/user/d/dsgalabe/GCCXML/gccxml-install/bin:$PATH # gcc-xml
 
 # setup Xerces-C++
 #export XERCESC_INSTALL_DIR=/atlas/users/dsgalabe/XERCES-C++/xerces-c-3.1.3-install
-export XERCESC_INSTALL_DIR=/atlas/users/dsgalabe/xerces-c-3.1.3/xerces-c-3.1.3-install
+##export XERCESC_INSTALL_DIR=/atlas/users/dsgalabe/xerces-c-3.1.3/xerces-c-3.1.3-install
 #export XERCESC_INCLUDE_DIR=/atlas/users/dsgalabe/xerces-c-3.1.3/xerces-c-3.1.3-install/include
 #export XERCESC_LIBRARY_DIR=/atlas/users/dsgalabe/xerces-c-3.1.3/xerces-c-3.1.3-install/lib
 #export LD_LIBRARY_PATH=$XERCESC_LIBRARY_DIR:$XERCESC_INCLUDE_DIR:$XERCESC_INSTALL_DIR/lib:$LD_LIBRARY_PATH # Xerces-C++
-export LD_LIBRARY_PATH=$XERCESC_INSTALL_DIR/lib:$LD_LIBRARY_PATH # Xerces-C++
+##export LD_LIBRARY_PATH=$XERCESC_INSTALL_DIR/lib:$LD_LIBRARY_PATH # Xerces-C++
 
 # setup CLHEP
-export CLHEP_INCLUDE_DIR=/atlas/users/dsgalabe/CLHEP/clhep_install/include
-export CLHEP_LIBRARY_DIR=/atlas/users/dsgalabe/CLHEP/clhep_install/lib
-export LD_LIBRARY_PATH=$CLHEP_INCLUDE_DIR:$CLHEP_LIBRARY_DIR:$LD_LIBRARY_PATH        
+#export CLHEP_INCLUDE_DIR=/atlas/users/dsgalabe/CLHEP/clhep_install/include
+export CLHEP_BASE_DIR=$CLHEP_DIR/
+export CLHEP_INCLUDE_DIR=$CLHEP_DIR/include/
+export CLHEP_LIBRARY_DIR=$CLHEP_DIR/lib/
+export CLHEP_LIB_DIR=$CLHEP_DIR/lib/
+export LD_LIBRARY_PATH=$CLHEP_INCLUDE_DIR:$CLHEP_LIBRARY_DIR:$LD_LIBRARY_PATH
 
 # # setup VGM
 # export VGM_INSTALL_DIR=/atlas/users/dsgalabe/VGM/vgm.4.3-install
 
 # setup t2k-nd280-upgrade
 export T2KND280UP=$1
-export T2KND280UP_SRC=$T2KND280UP/src/app/EffStudy 
+export T2KND280UP_SRC=$T2KND280UP/src/app/EffStudy
 export T2KND280UP_EVT=$T2KND280UP/src/app/nd280UpEvent
 export T2KND280UP_RUN=$2
