8f6ff8af4eb9d1e03674c88eb5e09d2cb2f91c1b
IWAMOTO Konosuke <kiwamoto@cw14.cc.kek.jp>
2018-12-27 17:07:46 +0900

    updated egamma separation macro and submit scripts

--- src/app/Reconstruction/Main.cc
+++ src/app/Reconstruction/Main.cc
@@ -2,15 +2,18 @@
 #include "LightYield.hh"
 #include "NeutronAnalysis.hh"
 #include "StoppingProton.hh"
+#include "EGammaSeparation.hh"
 
 int main(int argc,char** argv) {
   // 1) leave one or several analysis uncommented
   // 2) recompile
   // 3) run
 
-  //TrackReconstruction(argc, argv);
+  TrackReconstruction(argc, argv);
+  EGammaSeparation(argc, argv);
+  
   //LightYield(argc, argv);
-  NeutronAnalysis(argc, argv);
+  // NeutronAnalysis(argc, argv);
   //StoppingProton(argc, argv);
   
   return 1;
---
+++ src/app/Reconstruction/src/EGammaSeparation.hh
@@ -0,0 +1,6 @@
+#ifndef ND280UP_EGAMMASEP
+#define ND280UP_EGAMMASEP
+
+int EGammaSeparation(int argc,char** argv);
+
+#endif
