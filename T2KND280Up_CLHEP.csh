e9bad22ce68888871c8587ad6f3777706998d6d0
IWAMOTO Konosuke <kiwamoto@cw14.cc.kek.jp>
2018-12-27 16:15:36 +0900

    modified the setup shells

--- T2KND280Up_CLHEP.csh
+++ T2KND280Up_CLHEP.csh
@@ -1,46 +1,70 @@
-setenv SOFTWARE_DIR  /sps/t2k/mlamoure/dev/Upgrade
+#!/usr/bin/csh
+
+setenv SOFTWARE_DIR /gpfs/home/t2k/kiwamoto/ND280Upgrade/Software
 
 # setup GEANT4
-source ${SOFTWARE_DIR}/geant4.10.01.p03-install/bin/geant4.csh ${SOFTWARE_DIR}/geant4.10.01.p03-install/bin
+source ${SOFTWARE_DIR}/GEANT4-install/bin/geant4.csh ${SOFTWARE_DIR}/GEANT4-install/bin
 
-setenv G4INCLUDE ${SOFTWARE_DIR}/geant4.10.01.p03-install/include/Geant4/
-setenv GEANT4_INCLUDE ${SOFTWARE_DIR}/geant4.10.01.p03-install/include/Geant4/
-setenv GEANT4_INCLUDE_DIR ${SOFTWARE_DIR}/geant4.10.01.p03-install/include/Geant4/
-setenv GEANT4_INSTALL_BIN ${SOFTWARE_DIR}/geant4.10.01.p03-install/bin/
+setenv G4INCLUDE ${SOFTWARE_DIR}/GEANT4-install/include/Geant4/
+setenv GEANT4_INCLUDE ${SOFTWARE_DIR}/GEANT4-install/include/Geant4/
+setenv GEANT4_INCLUDE_DIR ${SOFTWARE_DIR}/GEANT4-install/include/Geant4/
+setenv GEANT4_INSTALL_BIN ${SOFTWARE_DIR}/GEANT4-install/bin/
 
-setenv G4LIB ${SOFTWARE_DIR}/geant4.10.01.p03-install/lib64
-setenv G4SYSTEM ${SOFTWARE_DIR}/geant4.10.01.p03-install/lib64
-setenv GEANT4_LIBRARY ${SOFTWARE_DIR}/geant4.10.01.p03-install/lib64
-setenv GEANT4_LIBRARY_DIR ${SOFTWARE_DIR}/geant4.10.01.p03-install/lib64
+setenv G4LIB ${SOFTWARE_DIR}/GEANT4-install/lib64
+setenv G4SYSTEM ${SOFTWARE_DIR}/GEANT4-install/lib64
+setenv GEANT4_LIBRARY ${SOFTWARE_DIR}/GEANT4-install/lib64
+setenv GEANT4_LIBRARY_DIR ${SOFTWARE_DIR}/GEANT4-install/lib64
 
 # setup ROOT
-source /sps/t2k/mlamoure/repository/v12r1/ROOT/v5r34p34n00/Linux-x86_64/root/bin/thisroot.csh
+source /gpfs/home/t2k/kiwamoto/ND280Upgrade/Software/root_v5.34.36/bin/thisroot.csh
 
 # setup CMT
-source /sps/t2k/mlamoure/CMT/setup.csh
-setenv CMTPATH ${SOFTWARE_DIR}/t2k-nd280-upgrade:/sps/t2k/mlamoure/repository/v12r1
+source /gpfs/home/t2k/kiwamoto/ND280Upgrade/Software/CMT/setup.csh
+setenv CMTPATH ${SOFTWARE_DIR}/t2k-nd280-upgrade:${SOFTWARE_DIR}/CMT
+
+#setup NEUT
+
+setenv CERN ${SOFTWARE_DIR}/CERNLIB
+setenv CERN_LEVEL 2005
+setenv CERN_ROOT ${CERN}/${CERN_LEVEL}
+setenv CERNLIB ${CERN_ROOT}/lib
+
+if ($?LD_LIBRARY_PATH) then
+setenv LD_LIBRARY_PATH ${CERNLIB}:${LD_LIBRARY_PATH}
+else
+setenv LD_LIBRARY_PATH ${CERNLIB}
+endif
+
+setenv NEUT_ROOT ${SOFTWARE_DIR}/neut_5.3.6
+setenv LD_LIBRARY_PATH ${NEUT_ROOT}/src/reweight:${LD_LIBRARY_PATH}
 
 # setup highlandUp
-source ${SOFTWARE_DIR}/t2k-nd280-upgrade/highlandUP/run_at_start_t2k.csh
+#source ${SOFTWARE_DIR}/t2k-nd280-upgrade/highlandUP/run_at_start_t2k.csh
+setenv CVSROOT :ext:anoncvs@repo.nd280.org:/home/trt2kmgr/ND280Repository
+setenv CVS_RSH ssh
+unset CVS_SERVER
 
 # setup Xerces-C++
-setenv XERCESC_INSTALL_DIR ${SOFTWARE_DIR}/xerces-c-3.1.3/xerces-c-3.1.3-install
-setenv LD_LIBRARY_PATH ${XERCESC_INSTALL_DIR}/lib:${LD_LIBRARY_PATH}
+#setenv XERCESC_INSTALL_DIR ${SOFTWARE_DIR}/xerces-c-3.1.3/xerces-c-3.1.3-install
+#setenv LD_LIBRARY_PATH ${XERCESC_INSTALL_DIR}/lib:${LD_LIBRARY_PATH}
 
 # setup CLHEP
-setenv CLHEP_BASE_DIR ${SOFTWARE_DIR}/CLHEP2.2.0.4-install/
-setenv CLHEP_INCLUDE_DIR ${SOFTWARE_DIR}/CLHEP2.2.0.4-install/include
-setenv CLHEP_LIBRARY_DIR ${SOFTWARE_DIR}/CLHEP2.2.0.4-install/lib
-setenv CLHEP_LIB_DIR ${SOFTWARE_DIR}/CLHEP2.2.0.4-install/lib
-
+setenv CLHEP_BASE_DIR ${SOFTWARE_DIR}/CLHEP-install/
+setenv CLHEP_INCLUDE_DIR ${SOFTWARE_DIR}/CLHEP-install/include
+setenv CLHEP_LIBRARY_DIR ${SOFTWARE_DIR}/CLHEP-install/lib
+setenv CLHEP_LIB_DIR ${SOFTWARE_DIR}/CLHEP-install/lib
 
 setenv LD_LIBRARY_PATH ${CLHEP_INCLUDE_DIR}:${CLHEP_LIBRARY_DIR}:${LD_LIBRARY_PATH}     
 
 # setup VGM
-setenv VGM_INSTALL_DIR ${SOFTWARE_DIR}/vgm.4.3-install
+#setenv VGM_INSTALL_DIR ${SOFTWARE_DIR}/vgm.4.3-install
 
 # setup t2k-nd280-upgrade
 setenv T2KND280UP ${SOFTWARE_DIR}/t2k-nd280-upgrade
 setenv T2KND280UP_SRC ${T2KND280UP}/src/app/EffStudy 
 setenv T2KND280UP_EVT ${T2KND280UP}/src/app/nd280UpEvent
-setenv T2KND280UP_RUN=$1
+setenv T2KND280UP_RUN /gpfs/fs03/t2k/beam/work/kiwamoto/ND280Upgrade/Run
+setenv T2KND280UP_REC ${T2KND280UP}/src/app/Reconstruction
+setenv T2KND280UP_ELPH /gpfs/fs03/t2k/beam/work/kiwamoto/ELPH_11122018
+
+echo "DONE T2KND280Up_CLHEP.csh"
--- T2KND280Up_CLHEP.sh
+++ T2KND280Up_CLHEP.sh
@@ -1,46 +1,70 @@
-#!bin/sh
+#!/usr/bin/sh
 
-# Specify all the environment variables here!
-export GEANT4_DIR=/Users/suvorov/SOFT/CERN/GEANT4/geant4.10.01.p03-install/
-export CLHEP_DIR=/Users/suvorov/SOFT/CERN/CLHEP/2.4.0.4-install/
-export CMT_DIR=/Users/suvorov/SOFT/OTHER/CMT/
-export ROOT_DIR=/Users/suvorov/SOFT/CERN/ROOT/root-6.12.06-build/
+export SOFTWARE_DIR=/gpfs/home/t2k/kiwamoto/ND280Upgrade/Software
 
 # setup GEANT4
-source $GEANT4_DIR/bin/geant4.sh
-export GEANT4_INCLUDE=$GEANT4_DIR/include/Geant4/
-export GEANT4_INSTALL_BIN=$GEANT4_DIR/bin/
-export GEANT4_LIBRARY=$GEANT4_DIR/lib/
-export GEANT4_LIBRARY_DIR=$GEANT4_DIR/lib/
+source ${SOFTWARE_DIR}/GEANT4-install/bin/geant4.sh
+
+export G4INCLUDE=${SOFTWARE_DIR}/GEANT4-install/include/Geant4/
+export GEANT4_INCLUDE=${SOFTWARE_DIR}/GEANT4-install/include/Geant4/
+export GEANT4_INCLUDE_DIR=${SOFTWARE_DIR}/GEANT4-install/include/Geant4/
+export GEANT4_INSTALL_BIN=${SOFTWARE_DIR}/GEANT4-install/bin/
+
+export G4LIB=${SOFTWARE_DIR}/GEANT4-install/lib64
+export G4SYSTEM=${SOFTWARE_DIR}/GEANT4-install/lib64
+export GEANT4_LIBRARY=${SOFTWARE_DIR}/GEANT4-install/lib64
+export GEANT4_LIBRARY_DIR=${SOFTWARE_DIR}/GEANT4-install/lib64
 
 # setup ROOT
-source $ROOT_DIR/bin/thisroot.sh
+source /gpfs/home/t2k/kiwamoto/ND280Upgrade/Software/root_v5.34.36/bin/thisroot.sh
 
 # setup CMT
-source $CMT_DIR/setup.sh
-export CMTPATH=$1
+source /gpfs/home/t2k/kiwamoto/ND280Upgrade/Software/CMT/setup.sh
+export CMTPATH=${SOFTWARE_DIR}/t2k-nd280-upgrade:${SOFTWARE_DIR}/CMT
+
+#setup NEUT
+
+export CERN=${SOFTWARE_DIR}/CERNLIB
+export CERN_LEVEL=2005
+export CERN_ROOT=${CERN}/${CERN_LEVEL}
+export CERNLIB=${CERN_ROOT}/lib
+
+if [ $?LD_LIBRARY_PATH ]; then
+   export LD_LIBRARY_PATH=${CERNLIB}:${LD_LIBRARY_PATH}
+else
+   export LD_LIBRARY_PATH=${CERNLIB}
+fi
+
+export NEUT_ROOT=${SOFTWARE_DIR}/neut_5.3.6
+export LD_LIBRARY_PATH=${NEUT_ROOT}/src/reweight:${LD_LIBRARY_PATH}
+
+# setup highlandUp
+#source ${SOFTWARE_DIR}/t2k-nd280-upgrade/highlandUP/run_at_start_t2k.sh
+export CVSROOT=:ext:anoncvs@repo.nd280.org:/home/trt2kmgr/ND280Repository
+export CVS_RSH=ssh
+unset CVS_SERVER
 
 # setup Xerces-C++
-#export XERCESC_INSTALL_DIR=/atlas/users/dsgalabe/XERCES-C++/xerces-c-3.1.3-install
-##export XERCESC_INSTALL_DIR=/atlas/users/dsgalabe/xerces-c-3.1.3/xerces-c-3.1.3-install
-#export XERCESC_INCLUDE_DIR=/atlas/users/dsgalabe/xerces-c-3.1.3/xerces-c-3.1.3-install/include
-#export XERCESC_LIBRARY_DIR=/atlas/users/dsgalabe/xerces-c-3.1.3/xerces-c-3.1.3-install/lib
-#export LD_LIBRARY_PATH=$XERCESC_LIBRARY_DIR:$XERCESC_INCLUDE_DIR:$XERCESC_INSTALL_DIR/lib:$LD_LIBRARY_PATH # Xerces-C++
-##export LD_LIBRARY_PATH=$XERCESC_INSTALL_DIR/lib:$LD_LIBRARY_PATH # Xerces-C++
+#export XERCESC_INSTALL_DIR=${SOFTWARE_DIR}/xerces-c-3.1.3/xerces-c-3.1.3-install
+#export LD_LIBRARY_PATH=${XERCESC_INSTALL_DIR}/lib:${LD_LIBRARY_PATH}
 
 # setup CLHEP
-#export CLHEP_INCLUDE_DIR=/atlas/users/dsgalabe/CLHEP/clhep_install/include
-export CLHEP_BASE_DIR=$CLHEP_DIR/
-export CLHEP_INCLUDE_DIR=$CLHEP_DIR/include/
-export CLHEP_LIBRARY_DIR=$CLHEP_DIR/lib/
-export CLHEP_LIB_DIR=$CLHEP_DIR/lib/
-export LD_LIBRARY_PATH=$CLHEP_INCLUDE_DIR:$CLHEP_LIBRARY_DIR:$LD_LIBRARY_PATH
+export CLHEP_BASE_DIR=${SOFTWARE_DIR}/CLHEP-install/
+export CLHEP_INCLUDE_DIR=${SOFTWARE_DIR}/CLHEP-install/include
+export CLHEP_LIBRARY_DIR=${SOFTWARE_DIR}/CLHEP-install/lib
+export CLHEP_LIB_DIR=${SOFTWARE_DIR}/CLHEP-install/lib
 
-# # setup VGM
-# export VGM_INSTALL_DIR=/atlas/users/dsgalabe/VGM/vgm.4.3-install
+export LD_LIBRARY_PATH=${CLHEP_INCLUDE_DIR}:${CLHEP_LIBRARY_DIR}:${LD_LIBRARY_PATH}     
+
+# setup VGM
+#export VGM_INSTALL_DIR=${SOFTWARE_DIR}/vgm.4.3-install
 
 # setup t2k-nd280-upgrade
-export T2KND280UP=$1
-export T2KND280UP_SRC=$T2KND280UP/src/app/EffStudy
-export T2KND280UP_EVT=$T2KND280UP/src/app/nd280UpEvent
-export T2KND280UP_RUN=$2
+export T2KND280UP=${SOFTWARE_DIR}/t2k-nd280-upgrade
+export T2KND280UP_SRC=${T2KND280UP}/src/app/EffStudy 
+export T2KND280UP_EVT=${T2KND280UP}/src/app/nd280UpEvent
+export T2KND280UP_RUN=/gpfs/fs03/t2k/beam/work/kiwamoto/ND280Upgrade/Run
+export T2KND280UP_REC=${T2KND280UP}/src/app/Reconstruction
+export T2KND280UP_ELPH=/gpfs/fs03/t2k/beam/work/kiwamoto/ELPH_11122018
+
+echo "DONE T2KND280Up_CLHEP.sh"
