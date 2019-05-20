296fb3c07bdb35f308cf6e865f87938489f664c9
Mathieu GUIGUE <mgguigue@gmail.com>
2019-05-20 10:17:47 +0200

    Building: moved headers of ND280Event into src folder; removed some custom libraries paths; switching to <EffStudyApp> instead of <Main>; everything builds.

--- src/app/nd280UpEvent/ND280SFGDVoxelSet.hh
+++ src/app/nd280UpEvent/ND280SFGDVoxelSet.hh
@@ -12,57 +12,65 @@
 
 using namespace ROOT::Math;
 
-class ND280SFGDVoxelSet: public TObject{
+class ND280SFGDVoxelSet : public TObject
+{
 
 protected:
-    std::vector  <ND280SFGDVoxel*>          fVoxels;    // All the voxels in this voxel set.
-    std::vector  <ND280SFGDHit*>            fHits;      // All the 2D hits associated to this voxel set. 
+    std::vector<ND280SFGDVoxel *> fVoxels; // All the voxels in this voxel set.
+    std::vector<ND280SFGDHit *> fHits;     // All the 2D hits associated to this voxel set.
     TNtuple *fData;
 
 public:
-
-    ND280SFGDVoxelSet(){
+    ND280SFGDVoxelSet()
+    {
         this->Init();
     };
 
-    virtual ~ND280SFGDVoxelSet(){
+    virtual ~ND280SFGDVoxelSet()
+    {
         this->Reset();
     };
 
     //-----Setters------
 
-    void SetVoxels (std::vector <ND280SFGDVoxel*> p_fVoxels) { fVoxels = p_fVoxels; }
-    void SetHits   (std::vector <ND280SFGDHit*> p_fHits)     { fHits = p_fHits;    }
+    void SetVoxels(std::vector<ND280SFGDVoxel *> p_fVoxels) { fVoxels = p_fVoxels; }
+    void SetHits(std::vector<ND280SFGDHit *> p_fHits) { fHits = p_fHits; }
 
     //------------------
 
     //-----Getters------
 
-    TNtuple* GetData() { return fData; }
-    std::vector  <ND280SFGDVoxel*>  GetVoxels()         { return fVoxels;      } 
-    ND280SFGDVoxel*            GetVoxel(Int_t p_N)      { return fVoxels[p_N]; }
-    std::vector  <ND280SFGDHit*>    GetHits()           { return fHits;        }
+    TNtuple *GetData() { return fData; }
+    std::vector<ND280SFGDVoxel *> GetVoxels() { return fVoxels; }
+    ND280SFGDVoxel *GetVoxel(Int_t p_N) { return fVoxels[p_N]; }
+    std::vector<ND280SFGDHit *> GetHits() { return fHits; }
 
-    std::vector <Double_t> GetX(){
-        std::vector <Double_t> auxVec;
+    std::vector<Double_t> GetX()
+    {
+        std::vector<Double_t> auxVec;
         auxVec.reserve(fVoxels.size());
-        for(UInt_t i=0; i<fVoxels.size(); i++){
+        for (UInt_t i = 0; i < fVoxels.size(); i++)
+        {
             auxVec.push_back(fVoxels[i]->GetX());
         }
         return auxVec;
     }
-    std::vector <Double_t> GetY(){
-        std::vector <Double_t> auxVec;
+    std::vector<Double_t> GetY()
+    {
+        std::vector<Double_t> auxVec;
         auxVec.reserve(fVoxels.size());
-        for(UInt_t i=0; i<fVoxels.size(); i++){
+        for (UInt_t i = 0; i < fVoxels.size(); i++)
+        {
             auxVec.push_back(fVoxels[i]->GetY());
         }
         return auxVec;
     }
-    std::vector <Double_t> GetZ(){
-        std::vector <Double_t> auxVec;
+    std::vector<Double_t> GetZ()
+    {
+        std::vector<Double_t> auxVec;
         auxVec.reserve(fVoxels.size());
-        for(UInt_t i=0; i<fVoxels.size(); i++){
+        for (UInt_t i = 0; i < fVoxels.size(); i++)
+        {
             auxVec.push_back(fVoxels[i]->GetZ());
         }
         return auxVec;
@@ -70,21 +78,23 @@ public:
 
     //------------------
 
-    void Init(){
+    void Init()
+    {
         gStyle->SetCanvasColor(0);
         gStyle->SetMarkerStyle(21);
         gStyle->SetMarkerSize(1.05);
         fData = new TNtuple("fData", "fData", "x:y:z:color");
     }
 
-    void DrawHits(Bool_t p_Wait, Bool_t p_All, TString canvName);    // p_Wait = kTRUE to update canvas and Wait for click, p_All used to define DrawEvent().
-    void DrawVoxels(Bool_t p_Wait, Bool_t p_All);                // p_Wait = kTRUE to update canvas and Wait for click
-    void DrawHitsAndVoxels();                                // Draws both hits and voxels for the event.
+    void DrawHits(Bool_t p_Wait, Bool_t p_All, TString canvName); // p_Wait = kTRUE to update canvas and Wait for click, p_All used to define DrawEvent().
+    void DrawVoxels(Bool_t p_Wait, Bool_t p_All);                 // p_Wait = kTRUE to update canvas and Wait for click
+    void DrawHitsAndVoxels();                                     // Draws both hits and voxels for the event.
 
-    void Reset(Option_t* /*option*/=""){
-    } 
+    void Reset(Option_t * /*option*/ = "")
+    {
+    }
 
-    ClassDef(ND280SFGDVoxelSet,1);
+    ClassDef(ND280SFGDVoxelSet, 1);
 };
 
 #endif
