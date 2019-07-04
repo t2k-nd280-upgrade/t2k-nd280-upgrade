78ab129426e5c9a829063872cb73ebaf832ad04f
Mathieu GUIGUE <mgguigue@gmail.com>
2019-07-04 12:22:35 +0200

    Adding env_setup: simplified version of the T2KND280 setup script that uses geant4-config and clhep-config

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## Installation
 
+0. Make sure you have sourced root and geant4 using "this_root.sh" and "geant4.sh"
+
+1. Source env_setup.sh: `source env_setup.sh` (additionnal GEANT4/CLHEP environment variables)
+
 1. Inside this folder, create an "installation" folder
 
 ```bash
---
+++ env_setup.sh
@@ -0,0 +1,13 @@
+# Setup Geant4 environment variables not defined by geant4.sh but needed by cmake
+export GEANT4_INCLUDE=$(geant4-config --prefix)/include/Geant4
+export GEANT4_INSTALL_BIN=$(geant4-config --prefix)/bin
+export GEANT4_LIBRARY=$(geant4-config --prefix)/lib
+export GEANT4_LIBRARY_DIR=$(geant4-config --prefix)/lib
+
+# Setup CLHEP environment variables needed by cmake
+# clhep-config returns some "" characters we need to get rid of...
+export CLHEP_BASE_DIR=$(clhep-config --prefix | sed s/\"//g)
+export CLHEP_INCLUDE_DIR=$CLHEP_BASE_DIR/include/
+export CLHEP_LIBRARY_DIR=$CLHEP_BASE_DIR/lib64/
+export CLHEP_LIB_DIR=$CLHEP_BASE_DIR/lib64/
+export LD_LIBRARY_PATH=$CLHEP_INCLUDE_DIR:$CLHEP_LIBRARY_DIR:$LD_LIBRARY_PATH
